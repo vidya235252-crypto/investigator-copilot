e4b3ea95417cdaf54c465856a35b62489788801b
vidya235252-crypto <vidya235252@gmail.com>
2026-08-27 10:29:02 +0530

    running tests

---
+++ backend/test_case.py
@@ -0,0 +1,15 @@
+import json
+import pandas as pd
+from evidence import case_builder
+from detection import ml_risk_model
+
+events = pd.read_csv("../data/raw/events.csv", parse_dates=["timestamp"])
+labels = pd.read_csv("../data/raw/labels.csv")
+
+model = ml_risk_model.load_model("detection/risk_model.joblib")
+
+account_id = labels[labels["scenario_type"] == "ato_with_payment_abuse"].iloc[0]["account_id"]
+account_events = events[events["account_id"] == account_id]
+case = case_builder.build_case(account_id, account_events, model)
+
+print(json.dumps(case, indent=2, default=str))
